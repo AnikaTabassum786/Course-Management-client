72497bbaa80c65104a93ffe5c408d08b11be698a
AnikaTabassum217 <tabassum.anika579@gmail.com>
2025-08-10 10:57:32 +0600

    Instruction page updated

--- src/pages/Instructors/Instructors.jsx
+++ src/pages/Instructors/Instructors.jsx
@@ -49,7 +49,40 @@ const Instructors = () => {
                     </div>
                 ))}
             </div>
+<div>
+                {/* <p className='text-center'>Why Lean From us?</p> */}
+                <div className="my-6 text-center font-bold text-3xl italic">
+                    Why Lean From Us?
+                </div>
+                <p className='px-4 text-justify text-lg font-normal'>At BidyaNiketon, we believe learning should be inspiring, practical, and result-driven. Our expert instructors bring years of real-world experience to the classroom, ensuring you gain not just theoretical knowledge, but the skills and confidence to apply it. With interactive lessons, personalized support, and a focus on your success, we make every learning journey meaningful and rewarding</p>
+            </div>
 
+            <div>
+                <p className="my-6 text-center font-bold text-3xl italic">What we Provide?</p>
+                <ul className="space-y-2 list-disc list-inside ">
+                    <li>
+                        <span className="font-semibold">Expert Instructors</span> – Learn from industry professionals with years of real-world experience.
+                    </li>
+                    <li>
+                        <span className="font-semibold">Comprehensive Courses</span> – Covering fundamentals to advanced skills in each subject.
+                    </li>
+                    <li>
+                        <span className="font-semibold">Hands-on Projects</span> – Apply your knowledge through practical, real-life projects.
+                    </li>
+                    <li>
+                        <span className="font-semibold">Flexible Learning</span> – Study at your own pace with lifetime access to course materials.
+                    </li>
+                    <li>
+                        <span className="font-semibold">Interactive Sessions</span> – Engage in live discussions, Q&A, and peer collaboration.
+                    </li>
+                    <li>
+                        <span className="font-semibold">Personalized Mentorship</span> – Get one-on-one guidance tailored to your goals.
+                    </li>
+                    <li>
+                        <span className="font-semibold">Career Support</span> – Resume building, interview prep, and job placement assistance.
+                    </li>
+                </ul>
+            </div>
             
         </>
 
